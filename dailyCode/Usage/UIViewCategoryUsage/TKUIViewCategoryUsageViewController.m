af977f393d4a6690d394d95f0054d1c226e27b2d
hanxiuhui <hanxiuhui@offcn.com>
2020-10-12 17:10:42 +0800

    <feature>虚线边框

--- dailyCode/Usage/UIViewCategoryUsage/TKUIViewCategoryUsageViewController.m
+++ dailyCode/Usage/UIViewCategoryUsage/TKUIViewCategoryUsageViewController.m
@@ -22,7 +22,7 @@
     
     
     UIView *view = [UIView new];
-    view.frame = CGRectMake(100, 200, 120, 30);
+    view.frame = CGRectMake(100, 50.f, 120, 30);
     view.backgroundColor = [UIColor redColor];
     [self.view addSubview:view];
 
@@ -38,7 +38,7 @@
 
     
     UILabel *testLbl = [UILabel new];
-    testLbl.frame = CGRectMake(100, 300, 120, 30);
+    testLbl.frame = CGRectMake(100, 50.f, 120, 30);
     testLbl.font = [UIFont systemFontOfSize:14];
     testLbl.textAlignment = NSTextAlignmentCenter;
     testLbl.text = @"看我的圆角边框";
@@ -51,6 +51,25 @@
     } else {
         NSLog(@"不响应");
     }
+    
+    
+    // 二维码容器视图
+    CGFloat qrContainerViewX = 20.f;
+    CGFloat qrContainerViewWidth = self.view.width - qrContainerViewX * 2.f;
+    CGFloat qrContainerViewHeight = 130.f;
+    UIView *qrContainerView = [[UIView alloc] initWithFrame:CGRectMake(qrContainerViewX, 150, qrContainerViewWidth, qrContainerViewHeight)];
+    qrContainerView.backgroundColor = UIColor.clearColor;
+    
+    // 绘制虚线边框
+    CAShapeLayer *borderLayer = [CAShapeLayer layer];
+    borderLayer.frame = qrContainerView.bounds;
+    borderLayer.path = [UIBezierPath bezierPathWithRect:borderLayer.bounds].CGPath;
+    borderLayer.lineWidth = 1. / [[UIScreen mainScreen] scale]; // 虚线宽
+    borderLayer.lineDashPattern = @[@2, @2]; // 虚线边框
+    borderLayer.fillColor = [UIColor clearColor].CGColor; // 填充色
+    borderLayer.strokeColor = [UIColor blackColor].CGColor; // 虚线着色
+    [qrContainerView.layer addSublayer:borderLayer];
+    [self.view addSubview:qrContainerView];
 }
 
 @end
