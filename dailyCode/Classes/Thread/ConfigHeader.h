c7426949a3def2f7e289294ba2fb901320adc417
hanxiuhui <hanxiuhui@offcn.com>
2020-12-14 15:36:05 +0800

    <test>add ConfigHeader file

---
+++ dailyCode/Classes/Thread/ConfigHeader.h
@@ -0,0 +1,13 @@
+//
+//  ConfigHeader.h
+//  dailyCode
+//
+//  Created by hello on 2020/12/14.
+//  Copyright © 2020 TK. All rights reserved.
+//
+
+#ifndef ConfigHeader_h
+#define ConfigHeader_h
+
+
+#endif /* ConfigHeader_h */
