90e0e43c3d037fdcda8a74ff263e00e2751bb7ad
hanxiuhui <hanxiuhui@offcn.com>
2020-12-14 15:20:43 +0800

    <test>增加测试头文件

---
+++ dailyCode/Classes/Thread/TankHeader.h
@@ -0,0 +1,14 @@
+//
+//  TankHeader.h
+//  dailyCode
+//
+//  Created by hello on 2020/12/14.
+//  Copyright © 2020 TK. All rights reserved.
+//
+
+#ifndef TankHeader_h
+#define TankHeader_h
+
+#define kDEBUGVALUE @"1"
+
+#endif /* TankHeader_h */
