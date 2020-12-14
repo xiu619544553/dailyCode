fc423aa8e73f94b34a9bd9311356a319cdfd4747
hanxiuhui <hanxiuhui@offcn.com>
2020-12-14 15:33:39 +0800

    Revert "<test>delete TankHeader file"
    This reverts commit 63c278429720584dddfc262232b152ec7ff4e22f.

---
+++ dailyCode/Classes/Thread/TestHeader.h
@@ -0,0 +1,14 @@
+//
+//  TestHeader.h
+//  dailyCode
+//
+//  Created by hello on 2020/12/14.
+//  Copyright © 2020 TK. All rights reserved.
+//
+
+#ifndef TestHeader_h
+#define TestHeader_h
+
+#define kNickName @"Alex"
+
+#endif /* TestHeader_h */
