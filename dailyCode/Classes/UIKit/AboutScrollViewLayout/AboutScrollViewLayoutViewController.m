75ffa79ca4db753ce3dc5f461a65d28b01dfca7d
hanxiuhui <hanxiuhui@offcn.com>
2021-05-20 15:33:58 +0800

    <feature>automaticallyAdjustsScrollView

--- dailyCode/Classes/UIKit/AboutScrollViewLayout/AboutScrollViewLayoutViewController.m
+++ dailyCode/Classes/UIKit/AboutScrollViewLayout/AboutScrollViewLayoutViewController.m
@@ -14,13 +14,14 @@
 
 @implementation AboutScrollViewLayoutViewController
 
+#pragma mark - Life Cycle
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.title = [NSString stringWithFormat:@"系统版本: %@", [UIDevice currentDevice].systemVersion];
     [self.view addSubview:self.tableView];
     
-
     /*
      contentInsetAdjustmentBehavior 是UIScrollView及其子类的属性，控制scrollView的显示的内容是否被导航栏及tabBar遮挡。
         * UIScrollViewContentInsetAdjustmentAutomatic : 默认值。scrollView或其子类不会被导航栏及tabbar遮挡
@@ -28,11 +29,12 @@
         * UIScrollViewContentInsetAdjustmentAlways    : contentInset总是由滚动视图的safeAreaInsets进行调整
      
      
-     
      automaticallyAdjustsScrollViewInsets 是 UIViewController的属性。
         作用：控制器是否自动调整 UIScrollView 的内边距，即 UIScrollView 显示内容是否允许被 navigationBar 及 tabbar 遮挡。
         * YES，默认值。scrollView或其子类不会被导航栏及tabbar遮挡
         * NO，scrollView或其子类会被导航栏及tabbar遮挡
+     官方文档：
+     这个属性的默认值是YES，它让容器视图控制器知道他们应该调整这个视图控制器视图的滚动视图插入，以考虑状态栏、搜索栏、导航栏、工具栏或标签栏所消耗的屏幕区域。如果您的视图控制器实现管理自己的滚动视图插入调整，则将此属性设置为NO。
      */
     
     if (@available(iOS 11.0, *)) {
@@ -48,64 +50,67 @@
     [super viewDidLayoutSubviews];
     self.tableView.frame = self.view.bounds;
     
+#pragma mark - 测试1
     /*
- 环境：
-    * iPhone 11 Pro Max
-    * 系统版本：13.4.1
-    * NavBar 和 tabbar均在；
-    * self.tableView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentAutomatic;
- 
- 结果：
-  <
- UITableView:
+     环境：
+     * iPhone 11 Pro Max
+     * 系统版本：13.4.1
+     * NavBar 和 tabbar均在；
+     * self.tableView.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentAutomatic;
+     
+     结果：
+     <
+     UITableView:
      frame = (0 0; 414 896);
      clipsToBounds = YES;
      contentOffset: {0, -88}; ----> 如果设置了UIScrollView不被导航栏遮挡，则Y轴偏移量为导航栏+状态栏高度的负数值，即 -(导航栏+状态栏)
      contentSize: {414, 1100};
      adjustedContentInset: {88, 0, 83, 0};  iOS11增加的属性：适配后的内边距
- 
      
- 打印结果：contentInset = {0, 0, 0, 0}
- >
- 
- 
- 
- 
- 
- 环境：
-    * iPhone 6S
-    * 系统版本：10.3.1
-    * NavBar 和 tabbar 均在；
-    * self.automaticallyAdjustsScrollViewInsets = YES;
- 
- 结果：
- <
- UITableView:
- frame = (0 0; 375 667);
- contentOffset: {0, -64};
- contentSize: {375, 1100}>
- 
- 打印结果：contentInset = {64, 0, 49, 0}
- 
- 
- 
- 
- contentInsetAdjustmentBehavior 或 automaticallyAdjustsScrollViewInsets 会影响 UIScrollView 的contentOffset的值：
-    * UIScrollViewContentInsetAdjustmentAutomatic 或 YES，则 contentOffset.y = -(导航栏+状态栏) - (contentInsets.top)
-    * UIScrollViewContentInsetAdjustmentNever 或 NO，则 contentOffset.y = - contentInsets.top
      
+     打印结果：contentInset = {0, 0, 0, 0}
+     >
      */
     
-    NSLog(@"%@", self.tableView);
-    NSLog(@"tableView.contentInset = %@", NSStringFromUIEdgeInsets(self.tableView.contentInset));
+#pragma mark - 测试2
+    /*
+     环境：
+     * iPhone 6S
+     * 系统版本：10.3.1
+     * NavBar 和 tabbar 均在；
+     * self.automaticallyAdjustsScrollViewInsets = YES;
+     
+     结果：
+     <
+     UITableView:
+     frame = (0 0; 375 667);
+     contentOffset: {0, -64};
+     contentSize: {375, 1100}>
+     
+     打印结果：contentInset = {64, 0, 49, 0}
+     */
+    
+#pragma mark - 结果
+    /*
+     contentInsetAdjustmentBehavior 或 automaticallyAdjustsScrollViewInsets 会影响 UIScrollView 的contentOffset的值：
+        * UIScrollViewContentInsetAdjustmentAutomatic 或 YES，则 contentOffset.y = -(导航栏+状态栏) - (contentInsets.top)
+        * UIScrollViewContentInsetAdjustmentNever 或 NO，则 contentOffset.y = - contentInsets.top
+     */
+    NSLog(@"\nstatusBarFrame: %@", NSStringFromCGRect([UIApplication sharedApplication].statusBarFrame));
+    NSLog(@"\ncontentInset: %@", NSStringFromUIEdgeInsets(self.tableView.contentInset));
+    NSLog(@"\nnavigationBarFrame: %@", NSStringFromCGRect(self.navigationController.navigationBar.frame));
+    
+    NSLog(@"\n%@\n", self.tableView);
 }
 
 #pragma mark - UITableViewDelegate
+
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 
 #pragma mark - UITableViewDataSource
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return 25;
 }
@@ -124,6 +129,7 @@
         _tableView.delegate = self;
         _tableView.dataSource = self;
         _tableView.tableFooterView = [UIView new];
+        _tableView.backgroundColor = UIColor.groupTableViewBackgroundColor;
         [_tableView registerClass:UITableViewCell.class forCellReuseIdentifier:NSStringFromClass(UITableViewCell.class)];
     }
     return _tableView;
