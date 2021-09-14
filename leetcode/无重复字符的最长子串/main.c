dea3cc9d4d3d57912b6bf05604f8628a3a1d34a3
hanxiuhui <hanxiuhui@offcn.com>
2021-09-14 19:32:04 +0800

    <feature>add  question & update docs

--- leetcode/无重复字符的最长子串/main.c
+++ leetcode/无重复字符的最长子串/main.c
@@ -44,7 +44,8 @@ int lengthOfLongestSubstring(char *s);
 
 int main(int argc, const char * argv[]) {
     
-    char string[20] = "abcdabcbb";
+//    char string[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char string[] = "ABCDDEFGHI";
     
     size_t len = strlen(string);
     printf("len=%zu\n", len);
@@ -81,17 +82,17 @@ int lengthOfLongestSubstring(char *s) {
     while (s[right] != '\0') {                   // ‘\0’是字符串的结束符号，由编译器自动加上
         
         if (posmap[s[right]] != 0) {
-            maxlen = max(maxlen,right -left);
+            maxlen = max(maxlen, right - left);
             if (left < posmap[s[right]]) {
                 left = posmap[s[right]];
             }
         }
         
-        posmap[s[right]] = right+1;
+        posmap[s[right]] = right + 1;
         right++;
     }
     
-    maxlen = max(maxlen,right -left);
+    maxlen = max(maxlen, right - left);
     return maxlen;
 }
 
@@ -99,13 +100,13 @@ int lengthOfLongestSubstring(char *s) {
 /*
  
  滑动窗口讲解：
- 因为题目告知我们的字符串由字母、数字、符合、空格组成，所以我们使用一个128（保证ASCLL码中的所有元素都可以被记录）大小的int型数组int map[128]来记录遍历到的字符串中每一个字符的位置，由于map初始化为{0}，为了避免第一个字母的位置和初始化的0产生歧义，所以我们记录字符串的位置从1开始，即1~n。
+ 因为题目告知我们的字符串由字母、数字、符合、空格组成，所以我们使用一个128（保证ASCII码中的所有元素都可以被记录）大小的int型数组int map[128]来记录遍历到的字符串中每一个字符的位置，由于map初始化为{0}，为了避免第一个字母的位置和初始化的0产生歧义，所以我们记录字符串的位置从1开始，即1~n。
 
  滑动窗口的关键步骤：
     ①维护当前窗口（当前不重复的子串）的大小：
-    我们使用两个位置指示器：left和right来维护一个不重复的字符串，但是这里要注意，left是不重复字符串的左边界，而right不是右边界，right-1才是右边界，right是正待处理的字符的位置，它有可能在[left,right-1]范围中的字符出现过了。
+        我们使用两个位置指示器：left和right来维护一个不重复的字符串，但是这里要注意，left是不重复字符串的左边界，而right不是右边界，right-1才是右边界，right是正待处理的字符的位置，它有可能在[left,right-1]范围中的字符出现过了。
     ②滑动到正确位置：
-    当right指向的字母已经在当前的不重复子串中出现过了,(通过map[s[right]] != 0来判断), 由于[left,right-1]已经是一个不重复的子串了，但是由于它不一定是最长的，可能后面还有一个比它长且也是不重复的子串，所以我们需要滑动到构成下一个不重复的子串的左边界，这个左边界就是当前[left,right-1]中的那个和right指示的字符相同的字符的下一个位置，因为只有将它“排除在外”，才能构成新的不重复子串。
+        当right指向的字母已经在当前的不重复子串中出现过了,(通过map[s[right]] != 0来判断), 由于[left, right-1]已经是一个不重复的子串了，但是由于它不一定是最长的，可能后面还有一个比它长且也是不重复的子串，所以我们需要滑动到构成下一个不重复的子串的左边界，这个左边界就是当前[left,right-1]中的那个和right指示的字符相同的字符的下一个位置，因为只有将它“排除在外”，才能构成新的不重复子串。
 
     ③代码里一个要理解清楚的地方：
  
@@ -122,10 +123,9 @@ int lengthOfLongestSubstring(char *s) {
  int map[128] = {0}；
  int left = 0;
  map[‘a’] = 1;
- int right =1;
+ int right = 1;
 
  遍历开始：
-
  left = 0; right = 1; map[‘b’] = 2;
  left = 0; right = 2; map[‘c’] = 3;
  left = 0; right = 3; 此时map[‘b’]已经 !=0( = 2)；,所以right所指的第二个b和当前不重复子串“abc”会构成重复，所以我们要滑动，那么left滑动到重复字符b的下一个位置c，即，left = 2（left=map[‘b]’）;
