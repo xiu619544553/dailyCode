746d077a38388dba58553c7d490424be86f84d78
hanxiuhui <hanxiuhui@offcn.com>
2021-09-18 10:34:28 +0800

    <feature>listView、构造语法

--- flutter/flutter_demo/lib/main.dart
+++ flutter/flutter_demo/lib/main.dart
@@ -1,29 +1,41 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_demo/test_push_pop.dart';
+import 'package:flutter_demo/model/base_widget.dart';
+import 'package:flutter_demo/model/syntax_demo.dart';
+import 'model/car.dart';
 
+
+/// 笔记
+/// Scaffold 类似于 UINavigationController
+/// Container 类似于 UIView
+/// Column 纵向布局
+/// row 横向布局
+/// Stack 层级布局
+/// Container(height: 10,) 可以用来作为容器 ，也可以用来占位设置间距
+/// SizedBox(height: 10) 只是占位置。
+/// RichText 富文本
+
+/// Dart 中没有析构函数，它是垃圾回收机制
+
+
+/// 小部件入口
 void main() {
-
   // 自定义页面
   runApp(MaterialApp(
     title: 'Navigation',
-    home: FirstScreen(),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Demo'),
+      ),
+      body: BaseWidgetDemo(),
+    ),
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+      primaryColor: Colors.lime,
+    ),
   ));
 }
 
 
-
-// 自定义 Widget
-class EACustomButton extends StatelessWidget {
-  final String label;
-
-  EACustomButton(this.label);
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return ElevatedButton(onPressed: () {}, child: Text(label));
-  }
-}
-
-
+/// 简单的测试入口
+// main() => testFunc();
