f618374372c8fa34fa1831786e8c7efb1ced9515
hanxiuhui <hanxiuhui@offcn.com>
2021-09-23 16:11:51 +0800

    <feature>检测 分贝

---
+++ dailyCode/dailyCode/Classes/Other/DecibelMeter/TKDecibelMeterViewController.h
@@ -0,0 +1,17 @@
+//
+//  TKDecibelMeterViewController.h
+//  dailyCode
+//
+//  Created by hello on 2021/9/23.
+//  Copyright © 2021 TK. All rights reserved.
+//
+
+#import "TKBaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TKDecibelMeterViewController : TKBaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ dailyCode/dailyCode/Classes/Other/DecibelMeter/TKDecibelMeterViewController.m
@@ -0,0 +1,32 @@
+//
+//  TKDecibelMeterViewController.m
+//  dailyCode
+//
+//  Created by hello on 2021/9/23.
+//  Copyright © 2021 TK. All rights reserved.
+//
+
+#import "TKDecibelMeterViewController.h"
+
+@interface TKDecibelMeterViewController ()
+
+@end
+
+@implementation TKDecibelMeterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
