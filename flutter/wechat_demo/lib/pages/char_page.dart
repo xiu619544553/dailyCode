d9f80a60493efb36c6a61bb6ffe5fc497d2766de
hanxiuhui <hanxiuhui@offcn.com>
2021-09-24 17:30:51 +0800

    <feature>微信页面右上角按钮

--- flutter/wechat_demo/lib/pages/char_page.dart
+++ flutter/wechat_demo/lib/pages/char_page.dart
@@ -1,5 +1,5 @@
-
 import 'package:flutter/material.dart';
+import 'package:wechat_demo/const.dart';
 
 class ChatPage extends StatefulWidget {
   const ChatPage({Key? key}) : super(key: key);
@@ -13,7 +13,20 @@ class _ChatPageState extends State<ChatPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('聊天'),
+        title: Text('微信'),
+        backgroundColor: WeChatThemeColor,
+        actions: [
+          GestureDetector(
+            child: Container(
+              color: Colors.red,
+              margin: EdgeInsets.only(right: 10),
+              child: Image(
+                image: AssetImage('images/wc_circle_add.png'),
+                width: 25,
+              ),
+            ),
+          ),
+        ],
       ),
       body: Center(
         child: Text('聊天'),
