59c86f74206754105a7f2c322d6ba7cf7a41819c
hanxiuhui <hanxiuhui@offcn.com>
2021-11-19 16:52:59 +0800

    <feature>添加 runloop 测试工程

---
+++ runloop_demo/runloop_demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  runloop_demo
+//
+//  Created by hello on 2021/11/19.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
