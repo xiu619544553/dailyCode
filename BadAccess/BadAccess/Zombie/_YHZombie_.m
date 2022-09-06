b66d755d71afc85a6165dd441a5a4792ce657093
hanxiuhui <hanxiuhui@offcn.com>
2022-09-06 14:50:08 +0800

    <docs>update docs

--- BadAccess/BadAccess/Zombie/_YHZombie_.m
+++ BadAccess/BadAccess/Zombie/_YHZombie_.m
@@ -12,7 +12,7 @@
 // 调用这个对象对的所有方法都hook住进行LOG
 - (id)forwardingTargetForSelector:(SEL)aSelector {
     NSLog(@"%p-[%@ %@]:%@",self ,[NSStringFromClass(self.class) componentsSeparatedByString:@"_YHZombie_"].lastObject, NSStringFromSelector(aSelector), @"向已经dealloc的对象发送了消息");
-    // 结束当前线程
+    // 结束当前进程
     abort();
 }
 
